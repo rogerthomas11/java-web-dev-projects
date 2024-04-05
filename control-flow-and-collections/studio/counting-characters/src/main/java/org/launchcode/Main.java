e0a833efe5ac1bd00abb5ad2a9d7555ed7c157ba
Roger Thomas <rogerthomas96@outlook.com>
2024-04-04 20:39:30 -0500

    worked on studio and exercises

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/Main.java
@@ -0,0 +1,27 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<Character, Integer> characterCounts = new HashMap<>();
+        String testString = "If the product of two terms is zero then common sense says at least on of the two terms has to be zero to start with.";
+        char[] charactersInString = testString.toCharArray();
+
+        for (char character : charactersInString) {
+            if (characterCounts.containsKey(character)) {
+                characterCounts.put(character, characterCounts.get(character) + 1);
+            }
+            else {
+                characterCounts.put(character, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> oneChar : characterCounts.entrySet()) {
+            System.out.println(oneChar.getKey() + ": " + oneChar.getValue());
+        }
+    }
+}
